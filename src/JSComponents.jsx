209c995ee419db5652cb128c08fae15f28683c04
Samvel Avanesov <samvel.avanesov@oregan.net>
2024-05-19 16:50:00 -0400

    js component example

--- src/JSComponents.jsx
+++ src/JSComponents.jsx
@@ -1,7 +1,7 @@
 export const JSComponents = ({ text }) => {
     return (
         <div>
-            <div>JS Components</div>
+            <div className="bg-red-800 text-white">JS Components</div>
             <div>{text}</div>
         </div>
     );
